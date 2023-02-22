7123d9a493b67adee7ccc0cb114be398ce7e577b
josscmorera <me@jossmorera.com>
2023-02-22 15:51:48 -0500

    fi

--- main.js
+++ main.js
@@ -2,8 +2,46 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin(user) {
+    if (user.userRole === 'ADMIN') {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+function getEmail(user) {
+    let fname = user.firstName.toLowerCase();
+    let lname = user.lastName.toLowerCase();
+    return fname + '.' + lname + '@codeimmersives.com';
+}
 
+function getPlaylistLength(playlist) {
+    return playlist.songs.length;
+  }
+
+function getHardestHomework(homework) {
+    if (homework.length === 0) {
+        return '';
+    }
+    let lowest = homework[0];
+    for (let i = 1; i < homework.length; i++) {
+        if (homework[i].averageScore < lowest.averageScore) {
+            lowest = homework[i];
+        }
+    }
+    return lowest.name;
+}
+
+function createPhonebook(names, numbers) {
+    let phonebook = {};
+    for (let i = 0; i < names.length; i++) {
+      let name = names[i];
+      let number = numbers[i];
+      phonebook[name] = number;
+    }
+    return phonebook;
+  }
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
